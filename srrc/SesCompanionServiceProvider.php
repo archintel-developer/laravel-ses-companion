8528848a60fd416c8df8254b0a4c6e6acff9665c
Seth <seth.abaquita@executivemosaic.com>
2019-10-11 11:28:55 +0800

    first commit

---
+++ srrc/SesCompanionServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ArchintelDev\SesCompanion;
+
+use Illuminate\Support\ServiceProvider;
+
+class SesCompanionProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
